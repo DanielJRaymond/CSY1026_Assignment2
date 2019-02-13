9128ff9afc51de94eba8b50893b5f7875ff05f1c
DanielJRaymond <daniel.raymond16@my.northampton.ac.uk>
2019-02-13 12:20:17 +0000

    create

--- create_51.sql
+++ create_51.sql
@@ -15,3 +15,38 @@ task_id		NUMBER(4),
 start_date	DATE NOT NULL,
 end_date	DATE NOT NULL,
 );
+
+
+
+
+
+
+
+
+
+CREATE TABLE project_stages (
+project_id			NUMBER(4),
+task_id				NUMBER(4)
+);
+
+
+
+
+CREATE TABLE meetings (
+meeting_id			NUMBER(4),
+meeting_date		DATE,
+meeting_location	VARCHAR2(30) NOT NULL
+);
+
+
+
+
+
+
+
+
+CREATE TABlE actions (
+meeting_id			NUMBER(4),
+owner				VARChAR2(25),
+deadline			DATE
+);
