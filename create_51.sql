1856e82359a0357a368d592be8c638522d4e3248
DanielJRaymond <daniel.raymond16@my.northampton.ac.uk>
2019-02-20 10:58:46 +0000

    create

--- create_51.sql
+++ create_51.sql
@@ -9,8 +9,8 @@ Harry Carter
 
 CREATE TABLE stages (
 task_id		NUMBER(4),	
-start_date	DATE NOT NULL,
-end_date	DATE NOT NULL,
+start_date	DATE,
+end_date	DATE
 );
 
 
@@ -34,7 +34,7 @@ project_id			NUMBER(4),
 task_id				NUMBER(4),
 meeting_id			NUMBER(4),
 meeting_date		DATE,
-meeting_location	VARCHAR2(30) NOT NULL
+meeting_location	VARCHAR2(30)
 );
 
 
@@ -46,6 +46,6 @@ meeting_location	VARCHAR2(30) NOT NULL
 
 CREATE TABlE actions (
 meeting_id			NUMBER(4),
-owner				VARChAR2(25),
+owner				VARCHAR2(25),
 deadline			DATE
 );
