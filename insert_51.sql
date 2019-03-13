c6a84e0f395d5c5181c797ca94b6dd9602e16fcb
DanielJRaymond <45375129+DanielJRaymond@users.noreply.github.com>
2019-03-13 12:44:29 +0000

    stages

--- insert_51.sql
+++ insert_51.sql
@@ -4,3 +4,31 @@ Daniel Raymond
 Gabriel Cruceanu
 Harry Carter
 */
+
+
+
+
+-- STAGES
+INSERT INTO stages
+(stage_id, stage_name, start_date, end_date)
+VALUES (seq_stages.NEXTVAL, 'DEVELOPMENT', '15-JAN-2019', '29-JAN-2019');
+
+INSERT INTO stages
+(stage_id, stage_name, start_date, end_date)
+VALUES (seq_stages.NEXTVAL, 'DEVELOPMENT', '3-FEB-2019', '17-FEB-2019');
+
+INSERT INTO stages
+(stage_id, stage_name, start_date, end_date)
+VALUES (seq_stages.NEXTVAL, 'DESIGN', '6-JAN-2019', '23-JAN-2019');
+
+INSERT INTO stages
+(stage_id, stage_name, start_date, end_date)
+VALUES (seq_stages.NEXTVAL, 'DESIGN', '6-MAR-2019', '25-MAR-2019');
+
+INSERT INTO stages
+(stage_id, stage_name, start_date, end_date)
+VALUES (seq_stages.NEXTVAL, 'TESTING', '7-FEB-2019', '26-FEB-2019');
+
+INSERT INTO stages
+(stage_id, stage_name, start_date, end_date)
+VALUES (seq_stages.NEXTVAL, 'TESTING', '6-JUN-2019', '28-JUN-2019');
